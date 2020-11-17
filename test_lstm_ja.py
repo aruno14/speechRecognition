a61fe8eaa7683ee989bb30fb668f1d2d88f21e1e
Arnaud <aruno14@outlook.com>
2020-11-17 22:45:51 +0900

    Some corrections and add test file for Japanese

--- test_lstm_ja.py
+++ test_lstm_ja.py
@@ -6,10 +6,13 @@ from tensorflow.keras.layers import LSTM, Input, Dense
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.utils import to_categorical
+import MeCab
 import matplotlib.pyplot as plt
 
-maxData = 10
-maxWords = 2000
+wakati = MeCab.Tagger("-Owakati -r /etc/mecabrc")
+
+maxData = 1000
+maxWord = 2000
 dataString = []
 string_max_length = 0
 with open('validated.tsv') as tsvfile:
@@ -19,20 +22,22 @@ with open('validated.tsv') as tsvfile:
     for row in reader:
         if len(dataString) >= maxData:
             break
-        sentence = ("start " + row[2] + " end").split(" ")
-        dataString.append(sentence)
-        string_max_length = max(len(sentence), string_max_length)
+        sentence = row[2].replace("。", "")
+        wordList = wakati.parse("start " + sentence + " end").split()
+        
+        dataString.append(wordList)
+        string_max_length = max(len(wordList), string_max_length)
 
 print("string_max_length: ", string_max_length)
 
-tokenizer = Tokenizer(num_words=maxWords, lower=True, oov_token="<rare>")
+tokenizer = Tokenizer(num_words=maxWord, lower=True, oov_token="<rare>")
 tokenizer.fit_on_texts(dataString)
 sequences = tokenizer.texts_to_sequences(dataString)
 
-vocab_size = min(len(tokenizer.word_index) + 1, maxWords)
+vocab_size = min(len(tokenizer.word_index) + 1, maxWord)
 print('Vocabulary Size: %d' % vocab_size)
 
-X, y = list(), list()
+X, Y = list(), list()
 for i, seq in enumerate(sequences):
     for j in range(1, len(seq)):
         in_seq, out_seq = seq[:j], seq[j]
@@ -40,7 +45,7 @@ for i, seq in enumerate(sequences):
         in_seq = to_categorical([in_seq], num_classes=vocab_size)[0]
         out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
         X.append(in_seq)
-        y.append(out_seq)
+        Y.append(out_seq)
 print('Total Sequences:', len(X))
 
 model = Sequential()
@@ -48,17 +53,18 @@ model.add(Input(shape=(string_max_length, vocab_size)))
 model.add(LSTM(units=32))
 model.add(Dense(units=vocab_size, activation='softmax'))
 model.summary()
-tf.keras.utils.plot_model(model, to_file='model_lstm.png', show_shapes=True)
+tf.keras.utils.plot_model(model, to_file='model_lstm_ja.png', show_shapes=True)
 
 epoch = 300
+batch_size = 512
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-history=model.fit(np.array(X), np.array(y), epochs=epoch)
+history=model.fit(np.asarray(X), np.asarray(Y), epochs=epoch, batch_size=batch_size)
 model.save_weights('model_lstm.h5')
 
 metrics = history.history
 plt.plot(history.epoch, metrics['loss'], metrics['accuracy'])
 plt.legend(['loss', 'accuracy'])
-plt.savefig("learning-lstm.png")
+plt.savefig("learning-lstm_ja.png")
 plt.show()
 plt.close()
 
@@ -68,7 +74,7 @@ def word_for_id(integer, tokenizer):
             return word
     return Nones
 
-input_str = "She is"
+input_str = "彼女 は"
 in_text = 'start ' +  input_str
 print("in_text: ", in_text)
 for i in range(string_max_length):
