9eefa1328f005dfb5f9d2640cd58e582337df583
Arnaud <aruno14@outlook.com>
2023-02-02 09:27:39 +0900

    Simplification

--- sentence_fr.py
+++ sentence_fr.py
@@ -4,19 +4,26 @@ import os
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Model, Sequential, load_model
-from tensorflow.keras.layers import LSTM, Embedding, Input, Dense, BatchNormalization, Conv2D, MaxPooling2D, Dropout, Flatten, TimeDistributed
+from tensorflow.keras.layers import LSTM, Embedding, Input, Dense, BatchNormalization, Conv2D, MaxPooling2D, Dropout, Flatten
 from tensorflow.keras.layers.experimental import preprocessing
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.utils import to_categorical
-import tensorflow_io as tfio
 import matplotlib.pyplot as plt
 
-maxData = 30
+tf.config.set_visible_devices([], 'GPU')
+
+reuse = True
+maxData = 64
+max_num_words=2000
+batch_size = 32
+epochs = 1
+latent_dim=512
 model_name = "model_sentence_fr"
 data_folder = "fr/"
-clips_folder = data_folder + "clips/"
+clips_folder = os.path.join(data_folder, "clips")
 block_length = 0.500#->500ms
+frame_length=512
 voice_max_length = int(8/block_length)#->8s
 print("voice_max_length:", voice_max_length)
 
@@ -26,44 +33,27 @@ def audioToTensor(filepath:str):
     audioSR = tf.get_static_value(audioSR)
     audio = tf.squeeze(audio, axis=-1)
     audio_length = int(audioSR * block_length)#20-> 50ms 40 -> 25ms
-    frame_step = int(audioSR * 0.008)# 128 when rate is 1600 -> 8ms
-    audio_clean = tf.constant([], tf.float32)
-    audio_length_clean = audioSR//20#50ms
-    for i in range(0, len(audio), audio_length_clean):
-        audio_slice = audio[i:i+audio_length_clean]
-        position = tfio.audio.trim(audio_slice, axis=0, epsilon=0.065)
-        start, stop=position[0], position[1]
-        if stop-start<5:
-            continue
-        audio_slice = audio_slice[start:stop]
-        audio_clean = tf.concat([audio_clean, audio_slice], 0)
+    frame_step = int(audioSR * 0.010)# 128 when rate is 1600 -> 8ms
 
-    if len(audio_clean)<audio_length*voice_max_length:
-        audio = tf.concat([np.zeros([audio_length*voice_max_length-len(audio_clean)]), audio_clean], 0)
+    required_length = audio_length*voice_max_length    
+    if len(audio)<required_length:
+        audio = tf.concat([np.zeros([required_length-len(audio)]), audio], 0)
     else:
-        audio = audio_clean[-(audio_length*voice_max_length):]
+        audio = audio[-required_length:]
 
-    spectrogram = tf.signal.stft(audio, frame_length=1024, frame_step=frame_step)
+    spectrogram = tf.signal.stft(audio, frame_length=frame_length, frame_step=frame_step)
     spectrogram = (tf.math.log(tf.abs(tf.math.real(spectrogram)))/tf.math.log(tf.constant(10, dtype=tf.float32))*20)-60
     spectrogram = tf.where(tf.math.is_nan(spectrogram), tf.zeros_like(spectrogram), spectrogram)
     spectrogram = tf.where(tf.math.is_inf(spectrogram), tf.zeros_like(spectrogram), spectrogram)
-    voice_length, voice = 0, []
-    nb_part = len(audio)//audio_length
-    part_length = len(spectrogram)//nb_part
-    partsCount = len(range(0, len(spectrogram)-part_length, int(part_length/2)))
-    parts = np.zeros((partsCount, part_length, 513))
-    for i, p in enumerate(range(0, len(spectrogram)-part_length, int(part_length/2))):
-        part = spectrogram[p:p+part_length]
-        parts[i] = part
-    return parts
+    return spectrogram
 
 testParts = audioToTensor(os.path.join(clips_folder, '6fffa205a339c1719abbdd83d8bfd9b11f5f07ca3c8c47fa55fec0b16325c0b2cf6e00a28f416702d507d9a9be60318b18533c1e9b498bc7fb356dab97c039e9.wav'))
-print(testParts.shape)
+print("testParts", testParts.shape)
 
 def loadDataFromFile(filepath):
     print("Load data from", filepath)
     dataVoice, dataString = [], []
-    string_max_lenght = 0
+    string_max_length = 0
     with open(filepath) as tsvfile:
       reader = csv.reader(tsvfile, delimiter='\t')
       next(reader)#skip header
@@ -75,36 +65,36 @@ def loadDataFromFile(filepath):
         if(len(wordList)<5):
             continue
         print(row[1], row[2], wordList)
-        string_max_lenght = max(len(wordList), string_max_lenght)
+        string_max_length = max(len(wordList), string_max_length)
         dataString.append(wordList)
         #dataVoice.append(row[1].replace(".mp3", '.wav'))
         dataVoice.append(row[1]+'.wav')
-    return dataVoice, dataString, string_max_lenght
+    return dataVoice, dataString, string_max_length
 
-dataVoice, dataString, string_max_lenght = loadDataFromFile(os.path.join(data_folder, 'train.tsv'))
+dataVoice, dataString, string_max_length = loadDataFromFile(os.path.join(data_folder, 'train.tsv'))
 
 print("voice_max_length: ", voice_max_length)
-print("string_max_lenght: ", string_max_lenght)
-tokenizer = Tokenizer(num_words=2000, lower=True, oov_token="<rare>")
+print("string_max_length: ", string_max_length)
+tokenizer = Tokenizer(num_words=max_num_words, lower=True, oov_token="<rare>")
 tokenizer.fit_on_texts(dataString)
 with io.open('tokenizer.txt', 'w', encoding='utf-8') as f:
     for word, index in tokenizer.word_index.items():
         f.write(word + ":" + str(index) + "\n")
-vocab_size = len(tokenizer.word_index) + 1
+vocab_size = min(len(tokenizer.word_index) + 1, max_num_words)
 print('Vocabulary Size: %d' % vocab_size)
+
 def prepareData(dataString, dataVoice):
     X_voice, X_string, Y_string = list(), list(), list()
-    for i, seq in enumerate(dataString):
+    all_seq = tokenizer.texts_to_sequences(dataString)
+    for i, seq in enumerate(all_seq):
         voice =  dataVoice[i]
-        seq = tokenizer.texts_to_sequences([seq])[0]
         for j in range(1, len(seq)):
-            in_seq, out_seq = seq[:j], seq[:j+1]
-            in_seq = pad_sequences([in_seq], maxlen=string_max_lenght-1)[0]
-            out_seq = pad_sequences([out_seq], maxlen=string_max_lenght-1)[0]
+            in_seq, out_seq = seq[:j], [seq[j]]
+            in_seq = pad_sequences([in_seq], maxlen=string_max_length)[0]
             out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
             X_voice.append(voice)
-            X_string.append(np.array(in_seq))
-            Y_string.append(np.array(out_seq))
+            X_string.append(in_seq)
+            Y_string.append(out_seq)
     return X_voice, X_string, Y_string
 
 X_voice, X_string, Y_string = prepareData(dataString, dataVoice)
@@ -114,15 +104,16 @@ class MySequence(tf.keras.utils.Sequence):
     def __init__(self, x_voice, x_string, y_string, batch_size):
         self.x_voice, self.x_string, self.y_string = x_voice, x_string, y_string
         self.batch_size = batch_size
+    
     def __len__(self):
-        return int(len(self.x_voice) / self.batch_size)
+        return len(self.x_voice) // self.batch_size
+
     def __getitem__(self, idx):
         batch_x_string = self.x_string[idx * self.batch_size:(idx + 1) * self.batch_size]
         batch_y_string = self.y_string[idx * self.batch_size:(idx + 1) * self.batch_size]
-        batch_x_voice = np.zeros((self.batch_size, testParts.shape[0], testParts.shape[1], testParts.shape[2]))
+        batch_x_voice = np.zeros((self.batch_size, testParts.shape[0], testParts.shape[1]))
         for i in range(0, batch_size):
-            voice = audioToTensor(os.path.join(clips_folder, self.x_voice[idx * self.batch_size + i]))
-            batch_x_voice[i] = voice
+            batch_x_voice[i] = audioToTensor(os.path.join(clips_folder, self.x_voice[idx * self.batch_size + i]))
         batch_x_string = np.array(batch_x_string)
         batch_y_string = np.array(batch_y_string)
         return [batch_x_voice, batch_x_string], batch_y_string
@@ -133,79 +124,55 @@ def word_for_id(integer, tokenizer):
             return word
     return None
 
-if os.path.exists(model_name):
+if os.path.exists(model_name) and reuse:
     print("Load: " + model_name)
     model = load_model(model_name)
 else:
-    latent_dim=256
-    encoder_inputs = Input(shape=(testParts.shape[0], None, None, 1))
-    preprocessing = TimeDistributed(preprocessing.Resizing(6, 129))(encoder_inputs)
-    normalization = TimeDistributed(BatchNormalization())(preprocessing)
-    conv2d = TimeDistributed(Conv2D(34, 3, activation='relu'))(normalization)
+    encoder_inputs = Input(shape=(testParts.shape[0], testParts.shape[1]))
+    encoder_inputs = tf.expand_dims(encoder_inputs, axis=-1)
+    
+    preprocessing = preprocessing.Resizing(400, testParts.shape[1]//2)(encoder_inputs)
+    normalization = BatchNormalization()(preprocessing)
+
+    split = tf.keras.layers.Reshape((voice_max_length, -1, normalization.shape[2], normalization.shape[3]))(normalization)
+
+    conv2d = TimeDistributed(Conv2D(34, 3, activation='relu'))(split)
     conv2d = TimeDistributed(Conv2D(64, 3, activation='relu'))(conv2d)
     maxpool = TimeDistributed(MaxPooling2D())(conv2d)
     dropout = TimeDistributed(Dropout(0.25))(maxpool)
     flatten = TimeDistributed(Flatten())(dropout)
+
     encoder_lstm = LSTM(units=latent_dim, return_state=True)
     encoder_outputs, state_h, state_c = encoder_lstm(flatten)
     encoder_states = [state_h, state_c]
 
-    decoder_inputs = Input(shape=(string_max_lenght-1))
-    dec_emb_layer = Embedding(vocab_size, latent_dim)
-    dec_emb = dec_emb_layer(decoder_inputs)
-    decoder_lstm = LSTM(units=latent_dim, return_sequences=True, return_state=True)
-    decoder_outputs, _, _ = decoder_lstm(dec_emb, initial_state=encoder_states)
-    decoder_dense = Dense(vocab_size, activation='softmax')
-    decoder_outputs = decoder_dense(decoder_outputs)
-    model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
-    model.summary()
-    tf.keras.utils.plot_model(model, to_file='model_sentence.png', show_shapes=True)
-    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc'])
+    decoder_inputs = Input(shape=(string_max_length))
+    dec_emb = Embedding(vocab_size, latent_dim)(decoder_inputs)
+    decoder_outputs = LSTM(units=latent_dim)(dec_emb, initial_state=encoder_states)
+    decoder_outputs = Dense(vocab_size, activation='softmax')(decoder_outputs)
 
-batch_size = 16
-epochs = 30
+    model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+    tf.keras.utils.plot_model(model, to_file='model_sentence.png', show_shapes=True)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+
 model.fit(MySequence(X_voice, X_string, Y_string, batch_size), epochs=epochs, batch_size=batch_size)
-#model.save_weights(model_name+'.h5')
 model.save(model_name)
 
-encoder_model = Model(encoder_inputs, encoder_states)
-encoder_model.summary(line_length=200)
-tf.keras.utils.plot_model(encoder_model, to_file='model_encoder.png', show_shapes=True)
-
-decoder_state_input_h = Input(shape=(latent_dim,))
-decoder_state_input_c = Input(shape=(latent_dim,))
-decoder_state_input = [decoder_state_input_h, decoder_state_input_c]
-dec_emb2= dec_emb_layer(decoder_inputs)
-decoder_outputs2, state_h2, state_c2 = decoder_lstm(dec_emb2, initial_state=decoder_state_input)
-decoder_states2 = [state_h2, state_c2]
-decoder_outputs2 = decoder_dense(decoder_outputs2)
-decoder_model = Model([decoder_inputs] + decoder_state_input, [decoder_outputs2] + decoder_states2)
-decoder_model.summary(line_length=200)
-tf.keras.utils.plot_model(decoder_model, to_file='model_decoder.png', show_shapes=True)
-
 def decode_sequence(input_seq):
-    states_value = encoder_model.predict(input_seq)
-    decoded_sentence = "start"
-    stop_condition = False
-    while not stop_condition:
-        sequence = tokenizer.texts_to_sequences([decoded_sentence.split(" ")])[0]
-        sequence = pad_sequences([sequence], maxlen=string_max_lenght-1)
-        sequence = np.array(sequence)
-        output_tokens, h, c = decoder_model.predict([sequence] + states_value)
-        sampled_token_index = np.argmax(output_tokens[0, -1, :])
-        sampled_word = word_for_id(sampled_token_index, tokenizer)
-        if(sampled_word==None):
-            sampled_word = "index:" + str(sampled_token_index)
-            sampled_token_index = 0
-        decoded_sentence += ' ' + sampled_word
-        if (sampled_word == 'end' or len(decoded_sentence.split(" ")) > string_max_lenght):
-            stop_condition = True
-    return decoded_sentence
+    decoded_sentence = tokenizer.texts_to_sequences(["start"])[0]
+    while len(decoded_sentence) < string_max_length:
+        sequence = pad_sequences([decoded_sentence], maxlen=string_max_length)
+        output_tokens = model.predict([input_seq, sequence], verbose=0)
+        sampled_token_index = np.argmax(output_tokens[0])
+        decoded_sentence.append(sampled_token_index)
+    return tokenizer.sequences_to_texts([decoded_sentence])[0]
 
 print("Test voice recognition")
-for test_path, test_string in [('6fffa205a339c1719abbdd83d8bfd9b11f5f07ca3c8c47fa55fec0b16325c0b2cf6e00a28f416702d507d9a9be60318b18533c1e9b498bc7fb356dab97c039e9.wav', "Ne prononcez pas devant nous le nom de cette femme monstrueuse."), ('common_voice_fr_19598936.wav', 'La Foire sur la place décrit les début du compositeur à Paris'), ('common_voice_fr_20268897.wav', 'Quand il revient à Paris, deux ans plus tard, la pépinière a été détruite'), ('common_voice_fr_19999318.wav', "Peu de chansons de cet album n'ont pas connu de reprises"), ('common_voice_fr_19733071.wav', 'La série avait été diffusée par la télévision algérienne quelques mois auparavant')]:
+for i in range(0, 5):
+    test_path = dataVoice[i]
+    test_string = dataString[i]
     print("test_string: ", test_string)
-    test_voice = audioToTensor(clips_folder+test_path)
+    test_voice = audioToTensor(os.path.join(clips_folder, test_path))
     print(np.array([test_voice]).shape)
     decoded_sentence = decode_sequence(np.array([test_voice]))
     print("decoded_sentence: ", decoded_sentence)
